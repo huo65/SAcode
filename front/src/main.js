f60edceca84df22035079adf80e0489b4d6912de
huo65 <1599272181@qq.com>
2026-06-17 22:48:26 +0800

    图标

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./locales";
+import "./style/fa-fallback.less";
 import "./style/common.less";
 import "./style/global.less";
 import "element-plus/es/components/message/style/css";
